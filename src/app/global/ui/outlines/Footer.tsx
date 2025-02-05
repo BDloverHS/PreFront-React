3bdbb0640209f8b3260183ed98efca942acdc923
BDloverHS <gustn020702naver.com@gmail.com>
2025-02-05 17:33:30 +0900

    헤더 및 푸터 기본틀 작업

---
+++ src/app/global/ui/outlines/Footer.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react'
+import { styled } from 'styled-components'
+
+const StyleFooter = styled.footer
+
+const Footer = () => {
+  return <StyleFooter>하단영역</StyleFooter>
+}
+
+export default React.memo(Footer)

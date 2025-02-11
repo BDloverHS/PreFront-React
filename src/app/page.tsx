9881d13ee07bb8f525eee9982b4decffbb1f1d5b
BDloverHS <gustn020702naver.com@gmail.com>
2025-02-11 10:30:34 +0900

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,5 @@
-import React from 'react'
-
 const MainPage = () => {
-  return (
-    <>
-      <h1>메인페이지!!</h1>
-      <div
-        style={{ background: '#3674B5', width: '100%', height: '20px' }}
-      ></div>
-      <div
-        style={{ background: '#344CB7', width: '100%', height: '20px' }}
-      ></div>
-      <div
-        style={{ background: '#16C47F', width: '100%', height: '20px' }}
-      ></div>
-      <div
-        style={{ background: '#B6FFA1', width: '100%', height: '20px' }}
-      ></div>
-    </>
-  )
+  return <h1>메인페이지!!</h1>
 }
 
 export default MainPage
